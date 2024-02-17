c27285c36020c2bfd771d56f189d804a3f28813b
Alexandre <156820724+Aleexqndre@users.noreply.github.com>
2024-02-17 05:01:51 +0100

    ajout de fonctionnalité, jeu en 5 round avec affichage du gagnant

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const computerSelection = getComputerChoice()
-const playerSelection = prompt(`Entrez un element rock/paper/scissors`).toLowerCase()
+let computerSelection
+let playerSelection 
+let playerPoint = 0;
+let computerPoint = 0;
 let whoWon
+let winer
 
 function getRandomInt() {
     return Math.floor((Math.random() * 3)+1);
@@ -21,16 +24,36 @@ function playRound(playerSelection,computerSelection){
     if((playerSelection === "rock" && computerSelection === "scissors")
     ||(playerSelection === "scissors" && computerSelection === "paper")
     ||(playerSelection === "paper" && computerSelection === "rock")){
-
+        playerPoint = playerPoint +1;
         return whoWon = `You won ! ${playerSelection} beats ${computerSelection}`
     }else if(playerSelection === computerSelection){
         return whoWon = `Tie, you both chose ${playerSelection}`
     }else{
+        computerPoint = computerPoint + 1;
         return whoWon = `You lose ! ${computerSelection} beats ${playerSelection}`
     }
 
 }
 
-console.log(playerSelection)
-console.log(computerSelection)
-console.log(playRound(playerSelection,computerSelection))
+function playGame(){
+    for(let i = 0 ; i < 5; i++){
+
+        computerSelection = getComputerChoice()
+        playerSelection = prompt(`Entrez un element Rock/paper/scissors`)
+        console.log(playerSelection)
+        console.log(computerSelection)
+        playRound(playerSelection,computerSelection)
+        console.log(whoWon)
+        console.log(playerPoint)
+        console.log(computerPoint)
+    }
+    if(playerPoint > computerPoint){
+        console.log(`You won ${playerPoint} - ${computerPoint}`)
+    }else{
+        console.log(`You lose ${playerPoint} - ${computerPoint}`)
+    }
+
+}
+
+playGame()
+
